6d032290ec73b98de5fe6ed51ea9007f83c05ea5
Nicholas Dimitriou <skeleynick@gmail.com>
2026-01-07 21:06:53 -0800

    part 1

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
-import express, { urlencoded } from 'express';
-import mysql, { setMaxParserCache } from 'mysql2/promise';
+import express from 'express';
+import mysql from 'mysql2/promise';
 
 const app = express();
 
@@ -9,115 +9,29 @@ app.use(express.static('public'));
 //for Express to get values using POST method
 app.use(express.urlencoded({extended:true}));
 
+
 //setting up database connection pool
 const pool = mysql.createPool({
-    host: "ysp9sse09kl0tzxj.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user: "ib3grk3xv6qvnip1",
-    password: "fn4g21gt0h5mjnx1",
-    database: "xtn5f2aybvuuamte",
+    host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "af5hlqhk9w73faj3",
+    password: "rcyt9yynfyyzgk4z",
+    database: "mjht3yl2c5wmggdk",
     connectionLimit: 10,
     waitForConnections: true
 });
 
+
 //routes
-app.get('/', (req, res) => {
-   res.render('home.ejs')
-});
-app.get('/authors', async(req, res) => {
-    let sql = `SELECT authorId, firstName, lastName
-               FROM authors
-               ORDER BY lastName ASC`
-    const [authors] = await pool.query(sql);
-   res.render('authors.ejs', {authors})
-});
-app.get('/quotes', async(req, res) => {
-    let sql = `SELECT quoteId, quote
-               FROM quotes`
-    const [quotes] = await pool.query(sql);
-   res.render('quotes.ejs', {quotes})
-});
-app.get('/updateQuote', async(req, res) => {
-    let quoteId = req.query.quoteId;
-    let sql = `SELECT *
-               FROM quotes
-               WHERE quoteId = ?`;
-    const [quoteInfo] = await pool.query(sql, [quoteId]);
-    
-    let sqlAuthors = `SELECT authorId, firstName, lastName
-                      FROM authors`;
-    const [authors] = await pool.query(sqlAuthors);   
-                  
-    res.render('updateQuote.ejs', {quoteInfo, authors})
+app.get('/', async (req, res) => {
+    let sql = `SELECT quote
+               FROM Quotes
+               ORDER BY RAND()
+               LIMIT 1`;
+  const [randomQuote] = await pool.query(sql);
+   res.render('home.ejs', {randomQuote});
 });
 
-app.post('/updateAuthor', async(req,res) => {
-    let fn = req.body.first;
-    let ln = req.body.last;
-    let birthPlace = req.body.bp;
-    let gender = req.body.sex;
-    let proffe = req.body.prof;
-    let img = req.body.url;
-    let info = req.body.bio;
-    let authorId = req.body.id;
-    let sql = `UPDATE authors
-               SET firstName = ?,
-                   lastName = ?,
-                   dob = ?,
-                   dod = ?, 
-                   sex = ?
-               WHERE authorId = ?`
-    let sqlParams = [fn, ln, dofb, dofd, authorId];
-    const [authorInfo] = await pool.query(sql, sqlParams);
-    res.redirect('/authors');
-})
-app.post('/addAuthor', async (req, res) => {
-    let fn = req.body.first;
-    let ln = req.body.last;
-    let birthPlace = req.body.bp;
-    let gender = req.body.sex;
-    let proffe = req.body.prof;
-    let img = req.body.url;
-    let info = req.body.bio;
-    let sql = `INSERT INTO authors
-                (firstName, lastName, dob, dod, sex, profession, country, portrait, biography)
-                VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`
-    let sqlParams = [fn, ln, dofb, dofd, gender,proffe, birthPlace, img, info];
-    const [rows] = await pool.query(sql, sqlParams);
-   res.render('addAuthor.ejs')
-});
-app.get('/addQuote', async(req, res) => {
-    let sql = `SELECT authorId, firstName, lastName
-               FROM authors
-               ORDER BY lastName`;
-    const [rows] = await pool.query(sql);
-    sql = `SELECT DISTINCT(category) 
-           FROM quotes
-           ORDER BY category`;
-    const [cate] = await pool.query(sql);
-   res.render('addQuote.ejs', {rows, cate})
-});
-app.post('/addQuote', async (req, res) => {
-    let id = req.body.authorId;
-    let q = req.body.quote;
-    let cates = req.body.Category;
-    let sql = `INSERT INTO quotes
-                (quote,authorId,category)
-                VALUES (?, ?, ?)`
-    let sqlParams = [q, id, cates];
-    const [insert] = await pool.query(sql, sqlParams);
-    sql = `SELECT authorId, firstName, lastName
-               FROM authors
-               ORDER BY lastName`;
-    const [rows] = await pool.query(sql);
-    sql = `SELECT DISTINCT(category) 
-           FROM quotes
-           ORDER BY category`;
-    const [cate] = await pool.query(sql);
-   res.render('addQuote.ejs', {rows, cate})
-});
-app.get('/addAuthor', (req, res) => {
-   res.render('addAuthor.ejs')
-});
+
 app.get("/dbTest", async(req, res) => {
    try {
         const [rows] = await pool.query("SELECT CURDATE()");
